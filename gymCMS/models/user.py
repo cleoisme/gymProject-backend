2102f918839441763dce010457c262098212d34e
Cleo Zhang <14207507+cleopapaya@users.noreply.github.com>
2023-10-27 00:07:19 -0700

    break classess

--- gymCMS/models/user.py
+++ gymCMS/models/user.py
@@ -4,16 +4,27 @@ from gymCMS.models.address import Address
 
 
 class User(models.Model):
+<<<<<<< HEAD
     is_admin = models.BooleanField()
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=128)  # This is the password field
+=======
+    is_admin = False
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+>>>>>>> 0f70e3a (break classess)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateTimeField(null=True)
     address = models.OneToOneField(Address, on_delete=models.PROTECT, primary_key=True)
 
+<<<<<<< HEAD
+=======
+
+class Client(User):
+>>>>>>> 0f70e3a (break classess)
     MEMBERSHIP_BRONZE = "B"
     MEMBERSHIP_SILVER = "S"
     MEMBERSHIP_GOLD = "G"
@@ -27,3 +38,12 @@ class User(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
+<<<<<<< HEAD
+=======
+
+
+class Admin(User):
+    is_admin = True
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+>>>>>>> 0f70e3a (break classess)
