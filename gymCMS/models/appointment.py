0f70e3a547fa4e1fa81d5b9cc891f273f3a4c7ab
Cleo Zhang <14207507+cleopapaya@users.noreply.github.com>
2023-10-27 00:07:19 -0700

    break classess

---
+++ gymCMS/models/appointment.py
@@ -0,0 +1,23 @@
+from django.db import models
+from gymCMS.models.promotion import Promotion
+from gymCMS.models.user import User
+
+
+class AppointmentType(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Appointment(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    last_update = models.DateTimeField(auto_now=True)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    app_type = models.ForeignKey(AppointmentType, on_delete=models.PROTECT)
+    trainer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="trainer"
+    )
+    trainee = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="trainee"
+    )
+    promotions = models.ManyToManyField(Promotion, null=True)
