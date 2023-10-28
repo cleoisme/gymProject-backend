55ef2ea0e1b0a86c29d19b1b45e5c773df35a627
Cleo Zhang <14207507+cleopapaya@users.noreply.github.com>
2023-10-27 17:20:00 -0700

    add routing for notificationSender

--- notificationSender/urls.py
+++ notificationSender/urls.py
@@ -1,7 +1,28 @@
 from . import views
 from django.urls import path
+from .views import (
+    NotificationListView,
+    NotificationDetailView,
+    NotificationCreateView,
+    NotificationUpdateView,
+    NotificationDeleteView,
+)
 
 # URLConf
+
 urlpatterns = [
     path("hello/", views.check_project),
+    path("notifications/", NotificationListView, name="notification-list"),
+    path("notifications/<int:pk>/", NotificationDetailView, name="notification-detail"),
+    path("notifications/create/", NotificationCreateView, name="notification-create"),
+    path(
+        "notifications/<int:pk>/update/",
+        NotificationUpdateView,
+        name="notification-update",
+    ),
+    path(
+        "notifications/<int:pk>/delete/",
+        NotificationDeleteView,
+        name="notification-delete",
+    ),
 ]
