e08908efc948d83e91d3a2db559c0a485f14db7b
Cleo Zhang <14207507+cleopapaya@users.noreply.github.com>
2023-11-03 10:25:25 -0700

    fix the routing for gymCMS

--- gymCMS/urls.py
+++ gymCMS/urls.py
@@ -1,111 +1,127 @@
-from .views import views
 from django.urls import path
+from gymCMS.views.address import (
+    create_an_address,
+    delete_an_address,
+    list_all_addresses,
+    show_address_details,
+    update_an_address,
+)
+from gymCMS.views.appointment import (
+    create_an_appointment,
+    create_an_appointment_type,
+    delete_an_appointment,
+    list_all_appointment_types,
+    list_all_appointments,
+    show_an_appointment,
+    show_appointment_type_details,
+    update_an_appointment,
+)
+from gymCMS.views.post import (
+    create_an_post,
+    delete_a_post,
+    list_all_posts,
+    show_a_post,
+    update_a_post,
+)
 
-from .views import (
-    # address
-    AddressListView,
-    AddressDetailView,
-    AddressCreateView,
-    AddressUpdateView,
-    AddressDeleteView,
-    # Address Type
-    AppointmentTypeListView,
-    AppointmentTypeDetailView,
-    AppointmentCreateView,
-    AppointmentUpdateView,
-    AppointmentDeleteView,
-    # Post
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-    # Pomotion
-    PromotionListView,
-    PromotionDetailView,
-    PromotionCreateView,
-    PromotionUpdateView,
-    PromotionDeleteView,
-    # User
-    UserListView,
-    UserDetailView,
-    UserCreateView,
-    UserUpdateView,
-    UserDeleteView,
+from gymCMS.views.promotion import (
+    create_a_promotion,
+    delete_a_promotion,
+    list_all_promotions,
+    show_a_promotion,
+    update_a_promotion,
+)
+from gymCMS.views.user import (
+    create_a_user,
+    delete_a_user,
+    list_all_users,
+    show_user_details,
+    update_a_user,
 )
 
 # URLConf
-
 urlpatterns = [
-    path("hello/", views.check_project),
+    # path("hello/", views.check_project),
     # address
-    path("address/", AddressListView, name="address-list"),
-    path("address/<int:pk>/", AddressDetailView, name="address-detail"),
-    path("address/create/", AddressCreateView, name="address-create"),
+    path("addresses/", list_all_addresses, name="address-list"),
+    path("addresses/<int:pk>/", show_address_details, name="address-detail"),
+    path("address/create/", create_an_address, name="address-create"),
     path(
-        "address/<int:pk>/update/",
-        AddressUpdateView,
+        "addresses/<int:pk>/update/",
+        update_an_address,
         name="address-update",
     ),
     path(
-        "address/<int:pk>/delete/",
-        AddressDeleteView,
+        "addresses/<int:pk>/delete/",
+        delete_an_address,
         name="address-delete",
     ),
-    # Address Type
-    path("addresstype/", AppointmentTypeListView, name="addresstype-list"),
-    path("addresstype/<int:pk>/", AppointmentTypeDetailView, name="addresstype-detail"),
-    path("addresstype/create/", AppointmentCreateView, name="addresstype-create"),
+    # Appointment Type
+    path("appointmenttypes/", list_all_appointment_types, name="addresstype-list"),
     path(
-        "addresstype/<int:pk>/update/",
-        AppointmentUpdateView,
-        name="addresstype-update",
+        "appointmenttypes/<int:pk>/",
+        show_appointment_type_details,
+        name="appointmenttype-detail",
     ),
     path(
-        "addresstype/<int:pk>/delete/",
-        AppointmentDeleteView,
-        name="addresstype-delete",
+        "appointmenttype/create/",
+        create_an_appointment_type,
+        name="appointmenttype-create",
+    ),
+    # Appointment
+    path("appointments/", list_all_appointments, name="appointment-list"),
+    path("appointments/<int:pk>/", show_an_appointment, name="appointment-details"),
+    path("appointment/create/", create_an_appointment, name="appointment-create"),
+    path(
+        "appointments/<int:pk>/update/",
+        update_an_appointment,
+        name="appointment-update",
+    ),
+    path(
+        "appointments/<int:pk>/delete/",
+        delete_an_appointment,
+        name="appointment-delete",
     ),
     # Post
-    path("post/", PostListView, name="post-list"),
-    path("post/<int:pk>/", PostDetailView, name="post-detail"),
-    path("post/create/", PostCreateView, name="post-create"),
+    path("posts/", list_all_posts, name="post-list"),
+    path("posts/<int:pk>/", show_a_post, name="post-detail"),
+    path("post/create/", create_an_post, name="post-create"),
     path(
-        "post/<int:pk>/update/",
-        PostUpdateView,
+        "posts/<int:pk>/update/",
+        update_a_post,
         name="post-update",
     ),
     path(
-        "post/<int:pk>/delete/",
-        PostDeleteView,
+        "posts/<int:pk>/delete/",
+        delete_a_post,
         name="post-delete",
     ),
     # Pomotion
-    path("promotion/", PromotionListView, name="promotion-list"),
-    path("promotion/<int:pk>/", PromotionDetailView, name="promotion-detail"),
-    path("promotion/create/", PromotionCreateView, name="promotion-create"),
+    path("promotions/", list_all_promotions, name="promotion-list"),
+    path("promotions/<int:pk>/", show_a_promotion, name="promotion-detail"),
+    path("promotion/create/", create_a_promotion, name="promotion-create"),
     path(
-        "promotion/<int:pk>/update/",
-        PromotionUpdateView,
+        "promotions/<int:pk>/update/",
+        update_a_promotion,
         name="promotion-update",
     ),
     path(
-        "promotion/<int:pk>/delete/",
-        PromotionDeleteView,
+        "promotions/<int:pk>/delete/",
+        delete_a_promotion,
         name="promotion-delete",
     ),
     # User
-    path("user/", UserListView, name="user-list"),
-    path("user/<int:pk>/", UserDetailView, name="user-detail"),
-    path("user/create/", UserCreateView, name="user-create"),
+    path("users/", list_all_users, name="user-list"),
+    path("users/<int:pk>/", show_user_details, name="user-detail"),
+    path("user/create/", create_a_user, name="user-create"),
     path(
-        "user/<int:pk>/update/",
-        UserUpdateView,
+        "users/<int:pk>/update/",
+        update_a_user,
         name="user-update",
     ),
     path(
-        "user/<int:pk>/delete/",
-        UserDeleteView,
+        "users/<int:pk>/delete/",
+        delete_a_user,
         name="user-delete",
     ),
 ]
