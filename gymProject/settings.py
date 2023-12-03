40b5a103ef4dc09b9f8f15979c98b093e3074595
Cleo Zhang <14207507+cleopapaya@users.noreply.github.com>
2023-12-03 09:35:27 -0800

    update database dependencies

--- gymProject/settings.py
+++ gymProject/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "gymProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # For development only
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "gymProject",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "storefront",
     }
 }
 
